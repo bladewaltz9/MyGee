663a03b50a11f31d9e9183e3f6530fc86a0a0c78
bladewaltz <bladewaltz@qq.com>
2023-12-08 17:29:57 +0800

    feat: implement error recovery

--- main.go
+++ main.go
@@ -3,16 +3,10 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"text/template"
 	"time"
 
 	"github.com/bladewaltz9/Gee"
 )
-
-type student struct {
-	Name string
-	Age  int8
-}
 
 func FormatAsDate(t time.Time) string {
 	year, month, day := t.Date()
@@ -21,33 +15,15 @@ func FormatAsDate(t time.Time) string {
 
 func main() {
 	gee := Gee.New()
-	gee.Use(Gee.Logger())
-
-	gee.SetFuncMap(template.FuncMap{
-		"FormatAsDate": FormatAsDate,
-	})
-	gee.LoadHTMLGlob("templates/*")
-	gee.Static("/assets", "./static")
-
-	stu1 := &student{Name: "tom", Age: 18}
-	stu2 := &student{Name: "jack", Age: 20}
+	gee.Use(Gee.Logger(), Gee.Recovery())
 
 	gee.GET("/", func(c *Gee.Context) {
-		c.HTML(http.StatusOK, "css.tmpl", nil)
+		c.String(http.StatusOK, "Hello Geektutu\n")
 	})
-
-	gee.GET("/students", func(c *Gee.Context) {
-		c.HTML(http.StatusOK, "arr.tmpl", Gee.H{
-			"title":  "Gee",
-			"stuArr": [2]*student{stu1, stu2},
-		})
-	})
-
-	gee.GET("/date", func(c *Gee.Context) {
-		c.HTML(http.StatusOK, "custom_func.tmpl", Gee.H{
-			"title": "Gee",
-			"now":   time.Date(2023, 12, 8, 0, 0, 0, 0, time.UTC),
-		})
+	// index out of range for testing Recovery()
+	gee.GET("/panic", func(c *Gee.Context) {
+		names := []string{"geektutu"}
+		c.String(http.StatusOK, names[100])
 	})
 
 	gee.Run(":10000")
